6830c195131d0f00cb61d8f78fdb740a5e1e33b2
amit123ak <41484488+amit123ak@users.noreply.github.com>
2025-09-10 22:41:14 +0530

    Create Factorial of the given number .java
    Create Factorial of the given number .java

---
+++ Factorial of the given number .java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+       int n=5;
+  //Factorial of the given number:
+  int fact=1;
+  for(int i=1;i<=n;i++)
+  {
+   fact=fact*i;
+  }
+  System.out.println(fact);
+ 
+
+
+
+    }
+}
